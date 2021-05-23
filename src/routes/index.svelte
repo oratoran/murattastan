128c1efa2046b9f914812a761df3fdf745658582
Ozgur Dincer <ozgurdincer@kentajans.com>
2021-05-23 13:47:23 +0300

    ok

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -56,7 +56,7 @@
             <p>Marmara Üniversitesi - Yüksek Lisans</p>
         </div>
         <p class="text-base text-pink-700 pb-4">TECRÜBELERİM</p>
-        <p><button on:click="{nationwide}">Ulusal Şirketler<svg xmlns="http://www.w3.org/2000/svg" class="inline h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <p><button on:click="{nationwide}">Ulusal Şirketler <svg xmlns="http://www.w3.org/2000/svg" class="inline h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
           </svg></button>
         </p>
@@ -69,7 +69,7 @@
                 </svg></button>
           </div>
         {/if}
-        <p><button on:click="{intl}">Çok Uluslu Şirketler<svg xmlns="http://www.w3.org/2000/svg" class="inline h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <p><button on:click="{intl}">Çok Uluslu Şirketler<svg xmlns="http://www.w3.org/2000/svg" class="inline h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122" />
           </svg></button></p>
     </div>
